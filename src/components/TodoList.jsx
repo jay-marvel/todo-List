317a51ab096a81a440d6e2682ffbe8dbd09d336e
Danny Zhang <dtz24+248@drexel.edu>
2025-07-22 21:28:51 -0400

    reconfigured previous repo

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,30 @@
+import { TodoCard } from "./TodoCard";
+
+export function TodoList(props) {
+    const { todos, selectedTab } = props
+
+
+    const filterTodosList = selectedTab === 'All' ?
+        todos :
+        selectedTab === 'Completed' ?
+            todos.filter(val => val.complete) :
+            todos.filter(val => !val.complete)
+
+    return (
+        <>
+            {filterTodosList.map((todo, todoIndex) => {
+                const tempTodoIndex = todos.findIndex(val => val.input == todo.input)
+                console.log(tempTodoIndex)
+                
+                return (
+                    <TodoCard
+                        key={todoIndex}
+                        {...props}
+                        todoIndex={tempTodoIndex}
+                        todo={todo} />
+                )
+            })}
+
+        </>
+    )
+}
